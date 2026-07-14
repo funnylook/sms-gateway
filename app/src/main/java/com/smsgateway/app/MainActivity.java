7ab83ab5b70f5ca46d90ea416058b4c27ac56903
funnylook <83181211+funnylook@users.noreply.github.com>
2026-07-14 18:57:13 +0800

    fix: remove unused Nullable import

--- app/src/main/java/com/smsgateway/app/MainActivity.java
+++ app/src/main/java/com/smsgateway/app/MainActivity.java
@@ -12,7 +12,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
